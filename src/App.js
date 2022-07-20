bd4ed250a4d406502b08cb46fd62203e67568008
Matv1K <balahonovmatvej@gmail.com>
2022-07-20 20:46:09 +0300

    Implement drop zone logic and add remove button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState('');
+  const fileInputRef = useRef();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+      setPreview(reader.result);
+      }
+      reader.readAsDataURL(image)
+    } else {
+      setPreview('');
+    }
+  }, [image])
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    fileInputRef.current.click();
+  }
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file && file.type.substr(0, 5) === 'image') {
+      setImage(file);
+    } else {
+      setImage(null)
+    }
+
+  }
+
+  const removeHandler = (e) => {
+    e.preventDefault();
+    setImage(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+      {preview ? <div className='block' style={{
+        backgroundImage: `url(${preview})`
+      }}>
+        <button className='remove' onClick={removeHandler}>remove</button>
+      </div> : <button className="wrapper" onClick={handleClick} >Add image</button>}
+        <input type='file' 
+        style={{display: 'none'}} 
+        ref={fileInputRef} 
+        onChange={handleChange} 
+        accept="image/*"/>
+      </form>
     </div>
   );
 }
